97d1b3a34a346826b783e4c3d744efab74f69d2e
Ali Baig <mirzaalibaig0714@gmail.com>
2025-02-24 10:31:11 +0500

    all files included

---
+++ hooks/footer-extras.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 1.1.0
+    </div>
+    <strong>Libray Management System &nbsp;Brought To You By <a target="_blank" href="https://apexsoftbuild.rf.gd/"> Apex SoftBuild</a>.</strong>
+  </footer>
